c99d3ab46e6d4644f20af51aaebb021bb0c0f17e
Dulce Monica-Andreea <91493363+Dulcele@users.noreply.github.com>
2022-12-24 00:02:12 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <link rel="stylesheet" href="assets/css/style.css">
+
+  <title>Blog</title>
+</head>
+
+<body>
+  <header>
+    <div class="logo">
+      <h1 class="logo-text"><span>Blog</span>X</h1>
+    </div>
+    <i class="fa fa-bars menu-toggle"></i>
+    <ul class="nav">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+       
+      <li>
+        <a href="#">
+          <i class="fa fa-user"></i>
+          Dulce Monica
+          <i class="fa fa-chevron-down" style="font-size: .8em;"></i>
+        </a>
+        <ul>
+          <li><a href="#">Dashboard</a></li>
+          <li><a href="#" class="logout">Logout</a></li>
+        </ul>
+        <i class="gg-log-out"></i>
+      </li>
+    </ul>
+  </header>
+
+  <!-- Page Wrapper -->
+  <div class="page-wrapper">
+
+    <!-- Post Slider -->
+    <div class="post-slider">
+      <h1 class="slider-title">Trending</h1>
+      <i class="fas fa-chevron-left prev"></i>
+      <i class="fas fa-chevron-right next"></i>
+
+      <div class="post-wrapper">
+
+        <div class="post">
+          <img src="assets/images/GettyImages-1207514166.png" alt="" class="slider-image">
+          <div class="post-info">
+            <h4><a href="single.html">Why does it hurt so much when we get ghosted? A psychologist explains</a></h4>
+            <i class="far fa-user"> Michelle Drouin</i>
+            &nbsp;
+            <i class="far fa-calendar"> Sept 18, 2022</i>
+          </div>
+        </div>
+
+        <div class="post">
+          <img src="assets/images/img.png.png" alt="" class="slider-image">
+          <div class="post-info">
+            <h4><a href="single.html">Gallery: During lockdown, this street photographer created a vibrant, surreal world    </a></h4>
+            <i class="far fa-user"> Paige Drownie</i>
+            &nbsp;
+            <i class="far fa-calendar"> May 31, 2022</i>
+          </div>
+        </div>
+
+        <div class="post">
+          <img src="assets/images/what-is-leadership.png" alt="" class="slider-image">
+          <div class="post-info">
+            <h4><a href="single.html">5 ways to show you can lead — even when you don t have a leadership role</a></h4>
+            <i class="far fa-user">  David Burkus</i>
+            &nbsp;
+            <i class="far fa-calendar"> May 5, 2022</i>
+          </div>
+        </div>
+
+        <div class="post">
+          <img src="assets/images/cute-girl.png" alt="" class="slider-image">
+          <div class="post-info">
+            <h4><a href="single.html">How to have better conversations on social media (really!)</a></h4>
+            <i class="far fa-user"> Hayley Caldwell</i>
+            &nbsp;
+            <i class="far fa-calendar">Nov 17, 2022</i>
+          </div>
+        </div>
+
+        <div class="post">
+          <img src="assets/images/sad.png" alt="" class="slider-image">
+          <div class="post-info">
+            <h4><a href="single.html">Even gritty people get discouraged</a></h4>
+            <i class="far fa-user"> Angela Duckworth PhD</i>
+            &nbsp;
+            <i class="far fa-calendar"> Nov 2, 2022 </i>
+          </div>
+        </div>
+
+
+      </div>
+
+    </div>
+    <!-- // Post Slider -->
+
+    <!-- Content -->
+    <div class="content clearfix">
+
+      <!-- Main Content -->
+      <div class="main-content">
+        <h1 class="recent-post-title">Recent Posts</h1>
+
+        <div class="post clearfix">
+          <img src="assets/images/pelle-pigmentata.png" alt="" class="post-image">
+          <div class="post-preview">
+            <h2><a href="single.hmtl">Leather is bad for animals and the planet — but what if we made it in a lab?</a></h2>
+            <i class="far fa-user"> Laura Pitcher</i>
+            &nbsp;
+            <i class="far fa-calendar"> Dec 1, 2022</i>
+            <p class="preview-text">
+            The fashion industry contributes around 8 to 10 percent of global greenhouse gas emissions — and one of the worst culprits is the leather industry.
+            </p>
+            <a href="single.html" class="btn read-more">Read More</a>
+          </div>
+        </div>
+
+        <div class="post clearfix">
+          <img src="assets/images/The-6-Great-Misconceptions-of-a-Trust.png" alt="" class="post-image">
+          <div class="post-preview">
+            <h2><a href="single.hmtl">7 ways to build greater trust in all of your partnerships</a></h2>
+            <i class="far fa-user"> Jean Oelwang</i>
+            &nbsp;
+            <i class="far fa-calendar"> Dec 12, 2022</i>
+            <p class="preview-text">
+            Quick question: How many relationships do you currently have where you feel like the other person has your back? And how would you like to have more of them? 
+            </p>
+            <a href="single.html" class="btn read-more">Read More</a>
+          </div>
+        </div>
+        <div class="post clearfix">
+          <img src="assets/images/love-life-1280x720.png" alt="" class="post-image">
+          <div class="post-preview">
+            <h2><a href="single.hmtl">4 ways that climate change could be affecting your love life</a></h2>
+            <i class="far fa-user"> Carla M. Delgado</i>
+            &nbsp;
+            <i class="far fa-calendar"> Dec 15, 2022</i>
+            <p class="preview-text">
+            The climate crisis is the single biggest threat facing humanity.
+            </p>
+            <a href="single.html" class="btn read-more">Read More</a>
+          </div>
+        </div>
+      </div>
+      <!-- // Main Content -->
+
+      <div class="sidebar">
+
+        <div class="section search">
+          <h2 class="section-title">Search</h2>
+          <form action="index.html" method="post">
+            <input type="text" name="search-term" class="fas fa-search" placeholder="...">
+            <i class="fas fa-search"></i>
+          </form>
+        </div>
+
+
+        <div class="section topics">
+          <h2 class="section-title">Topics</h2>
+          <ul>
+            <li><a href="#">Business</a></li>
+            <li><a href="#">Arts+Desing</a></li>
+            <li><a href="#">Science</a></li>
+            <li><a href="#">We Humans</a></li>
+          </ul>
+        </div>
+
+      </div>
+
+    </div>
+    <!-- // Content -->
+
+  </div>
+
+  <!-- footer -->
+  <div class="footer">
+    <div class="footer-content">
+
+      <div class="footer-section about">
+        <h1 class="logo-text"><span>Blog</span>X</h1>
+        <p>
+        BlogX is a website for both writers and readers passionate about art, science, business etc.
+        Here you can find the newest as well as the most authentic blogs on the internet.
+        </p>
+        <div class="contact">
+          <span><i class="fas fa-phone"></i> &nbsp; 0766245272</span>
+          <span><i class="fas fa-envelope"></i> &nbsp; info@blogx.com</span>
+        </div>
+        <div class="socials">     
+          <a href="#"><i class="fab fa-instagram"></i></a>
+        </div>
+      </div>
+
+      <div class="footer-section links">
+        <h2>Quick Links</h2>
+        <br>
+        <ul>
+          <a href="#">
+            <li>Gallery</li>
+          </a>
+          <a href="#">
+            <li>Terms and Conditions</li>
+          </a>
+        </ul>
+      </div>
+
+      <div class="footer-section contact-form">
+        <h2>Contact us</h2>
+        <br>
+        <form action="index.html" method="post">
+          <input type="email" name="email" class="text-input contact-input" placeholder="E-mail">
+          <textarea rows="4" name="message" class="text-input contact-input" placeholder="Your message..."></textarea>
+          <button type="submit" class="btn btn-big contact-btn">
+            <i class="fas fa-envelope"></i>
+            Send
+          </button>
+        </form>
+      </div>
+
+    </div>
+    </div>
+  </div>
+  <!-- // footer -->
+
+
+  <!-- JQuery -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+  <!-- Slick Carousel -->
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+  <!-- Custom Script -->
+  <script src="assets/js/scripts.js"></script>
+
+</body>
+
+</html>
---
+++ single.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Candal|Lora" rel="stylesheet">
+
+  <!-- Custom Styling -->
+  <link rel="stylesheet" href="assets/css/style.css">
+
+  <title>Single Post</title>
+</head>
+
+<body>
+  
+  <body>
+  <header>
+    <div class="logo">
+      <h1 class="logo-text"><span>Blog</span>X</h1>
+    </div>
+    <i class="fa fa-bars menu-toggle"></i>
+    <ul class="nav">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+       
+      <li>
+        <a href="#">
+          <i class="fa fa-user"></i>
+          Dulce Monica
+          <i class="fa fa-chevron-down" style="font-size: .8em;"></i>
+        </a>
+        <ul>
+          <li><a href="#">Dashboard</a></li>
+          <li><a href="#" class="logout">Logout</a></li>
+        </ul>
+        <i class="gg-log-out"></i>
+      </li>
+    </ul>
+  </header>
+
+  <!-- Page Wrapper -->
+  <div class="page-wrapper">
+
+    <!-- Content -->
+    <div class="content clearfix">
+
+      <!-- Main Content Wrapper -->
+      <div class="main-content-wrapper">
+        <div class="main-content single">
+          <h1 class="post-title">Why does it hurt so much when we get ghosted? A psychologist explains</h1>
+
+          <div class="post-content">
+            <p>As humans, we spend a good amount of time thinking about what other people think.
+            </p>
+            <p>Although this is not meant to be a controversial proposition, I am sure some of you are saying to yourselves, “No, I don’t do that,” or “I don’t care what other people think.”</p>
+            <p>This is understandable. Any opposition you might feel to this statement may stem from well-meaning advice givers, who in an effort to direct you away from self-criticism and anxiety, have assured you that what other people think of you does not matter.</p>
+            <p>To that I will say simply, “Yes, it does,” and urge you to read on as I explain why.
+
+</p>
+            <p>I’ll begin with a statement that we should all be able to endorse easily: When we act, it is likely and appropriate that we would think about how our actions affect others. For example, we think, “What does X think about what I just said?” Still, our thinking about others’ thoughts can also get more complex. We can widen our scope of inquiry by focusing on what someone thinks about the way we feel about someone else (e.g., “What does Y think I feel about X, considering what I just said?” or “What does Y think X feels about me, considering what I just said?”).
+            </p>
+            <p>These are not necessarily negative or maladaptive thoughts.</p>
+            <p>Quite to the contrary, these are complex ways of thinking — employed by the most socially skilled among us — to help us develop and maintain social connections. Although important, this way of thinking is not inborn; it is a sophisticated and critical skill we hone over our lifetimes.</p>
+            <p>As toddlers, we don’t consider our effects on other people. We make demands, cry and throw tantrums in an egocentric way, wanting only to satisfy our own needs and not caring about anyone else. It is only as we learn more about the world, when we develop something called “theory of mind” between the ages of about three to five, that we begin to recognize that other people have thoughts and feelings, and that our actions might affect them. By definition, the theory of mind is an ability to understand that both you and others have mental states and that others’ mental states might be different from your own and even different from what is reality.</p>
+            <p>This understanding of one’s own mental states gives individuals great power because once you realize you have thoughts and feelings, you can learn to control those thoughts and feelings. One of my favorite stories about the power that this skill affords comes from one of my university colleagues and a conversation he had with his daughter while driving her to preschool:
+            </p>
+            <p>“Dad, do you know what I’m thinking about?”</p>
+            <p>“No, Claire, what are you thinking about?”</p>
+            <p>“I’m thinking about Christmas.”</p>
+            <p>As a preschooler, Claire not only showed her dad that she had theory of mind but also demonstrated an impressive mastery of directing those thoughts to control her own internal world.
+            </p>
+            <p>I contend that this is the skill that the advice givers are trying to get you to develop. Essentially, they are not saying, “Don’t think about what other people think,” which would be almost impossible for those of us over the age of five who have even a bit of empathy. Rather, they are saying, “When you think about what others think, use it for its potential good and then dispose of it,” in a Marie Kondo kind of way.</p>
+            <p>Considering the average human has more than 6,000 thoughts per day, this type of cognitive purging would likely do us good, freeing up our mental resources for more positive thinking. But just like ridding our homes of unwanted items is a challenge for many of us, so is ridding our brains of unwanted thoughts and feelings. Particularly for those prone to anxiety and depression, worry and rumination — or stewing on negative thoughts associated with past events — is common and maladaptive.</p>
+            
+          </div>
+
+        </div>
+      </div>
+      <!-- // Main Content -->
+
+      <!-- Sidebar -->
+      <div class="sidebar single">
+
+        
+
+
+        <div class="section popular">
+          <h2 class="section-title">Trending</h2>
+
+          <div class="post clearfix">
+            <img src="assets/images/GettyImages-1207514166.png" alt="">
+            <a href="" class="title">
+              <h4>Why does it hurt so much when we get ghosted? A psychologist explains</h4>
+            </a>
+          </div>
+          <div class="post clearfix">
+            <img src="assets/images/img.png.png" alt="">
+            <a href="" class="title">
+              <h4>Gallery: During lockdown, this street photographer created a vibrant, surreal world starring his parents</h4>
+            </a>
+          </div>
+          <div class="post clearfix">
+            <img src="assets/images/what-is-leadership.png" alt="">
+            <a href="" class="title">
+              <h4>5 ways to show you can lead — even when you dont have a leadership role</h4>
+            </a>
+          </div>
+          <div class="post clearfix">
+            <img src="assets/images/sad.png" alt="">
+            <a href="" class="title">
+              <h4>Even gritty people get discouraged</h4>
+            </a>
+          </div>
+          <div class="post clearfix">
+            <img src="assets/images/cute-girl.png" alt="">
+            <a href="" class="title">
+              <h4>How to have better conversations on social media (really!)</h4>
+            </a>
+          </div>
+
+        </div>
+
+        <div class="section topics">
+          <h2 class="section-title">Topics</h2>
+          <ul>
+            <li><a href="#">Business</a></li>
+            <li><a href="#">Arts+Design</a></li>
+            <li><a href="#">Science</a></li>
+            <li><a href="#">We humans</a></li>
+          </ul>
+        </div>
+      </div>
+      <!-- // Sidebar -->
+
+    </div>
+    <!-- // Content -->
+
+  </div>
+  <!-- // Page Wrapper -->
+
+  <!-- footer -->
+  <div class="footer">
+    <div class="footer-content">
+
+      <div class="footer-section about">
+        <h1 class="logo-text"><span>Blog</span>X</h1>
+        <p>
+        BlogX is a website for both writers and readers passionate about art, science, business etc.
+        Here you can find the newest as well as the most authentic blogs on the internet.
+        </p>
+        <div class="contact">
+          <span><i class="fas fa-phone"></i> &nbsp; 0766245272</span>
+          <span><i class="fas fa-envelope"></i> &nbsp; info@blogx.com</span>
+        </div>
+        <div class="socials">     
+          <a href="#"><i class="fab fa-instagram"></i></a>
+        </div>
+      </div>
+
+      <div class="footer-section links">
+        <h2>Quick Links</h2>
+        <br>
+        <ul>
+          <a href="#">
+            <li>Gallery</li>
+          </a>
+          <a href="#">
+            <li>Terms and Conditions</li>
+          </a>
+        </ul>
+      </div>
+
+      <div class="footer-section contact-form">
+        <h2>Contact us</h2>
+        <br>
+        <form action="index.html" method="post">
+          <input type="email" name="email" class="text-input contact-input" placeholder="E-mail">
+          <textarea rows="4" name="message" class="text-input contact-input" placeholder="Your message..."></textarea>
+          <button type="submit" class="btn btn-big contact-btn">
+            <i class="fas fa-envelope"></i>
+            Send
+          </button>
+        </form>
+      </div>
+
+    </div>
+    </div>
+  </div>
+  <!-- // footer -->
+
+
+  <!-- JQuery -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+  <!-- Slick Carousel -->
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+  <!-- Custom Script -->
+  <script src="assets/js/scripts.js"></script>
+
+</body>
+
+</html>
